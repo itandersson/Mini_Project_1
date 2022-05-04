683b5ccab91b5cdab30914e22dd9176b9f1ac9dc
Joakim Andersson <joakim@ktv-sjobo.se>
2022-05-04 14:54:11 +0200

    Add in LaptopComputers and MobilePhones

---
+++ MobilePhones.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Mini_Project_1
+{
+
+	public class MobilePhones
+	{
+		private int endOfLife = 3;
+		private DateTime purchaseDate = DateTime.MinValue;
+		private int price = 0;
+		private string modelName = "";
+
+		public MobilePhones()
+		{
+		}
+	}
+
+	public class Iphone : MobilePhones
+	{
+
+		public Iphone()
+		{
+		}
+	}
+
+	public class Samsung : MobilePhones
+	{
+
+		public Samsung()
+		{
+		}
+	}
+
+	public class Nokia : MobilePhones
+	{
+
+		public Nokia()
+		{
+		}
+	}
+
+}
