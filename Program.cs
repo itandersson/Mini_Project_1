4e5bb42e35428e6a5cea48ef2214ae5092f83083
Joakim Andersson <joakim@ktv-sjobo.se>
2022-05-04 13:55:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mini_Project_1
+{
+    internal class AssetTracking
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
